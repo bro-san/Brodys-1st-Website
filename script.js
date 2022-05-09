946544c071b64a3cba479f443075a526652e351d
brody.sanford@gmail.com <brodysmac@Brodens-MacBook-Pro.local>
2022-05-08 21:25:37 -0400

    update latin section, added stickyNavBar

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content was added by JavaScript... Sweet!"
 document.querySelector('body').appendChild(h2)
+
+window.onscroll = function() {stickyNavBar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavBar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
